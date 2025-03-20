865a0af034201f9022426655cdc3ba24f1b966ec
AlexDev07 <alekeymurcia@gmail.com>
2025-03-19 23:30:27 -0600

    No le pusieron .js al llamar al archivo, no sé quien fue, pero ya lo arreglé

--- backend/src/routes/brandsRoutes.js
+++ backend/src/routes/brandsRoutes.js
@@ -1,7 +1,7 @@
 //este archivo sirve para definir que metoos del cud va atener mi ruta /api/products 
 
 import express from "express";
-import brandsController  from "../controllers/brandsCtrl";
+import brandsController  from "../controllers/brandsCtrl.js";
 
 const router = express.Router ()
 
