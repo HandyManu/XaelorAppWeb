aa10b24075761b44973cce5e190e738396c806b6
AlexDev07 <alekeymurcia@gmail.com>
2025-03-19 23:28:45 -0600

    Agregué las rutas

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,27 @@
+/**
+ * branches
+ * brands
+ * customers
+ * employees
+ * inventory
+ * memberships
+ * reviews
+ * sales
+ * watches
+ */
+
 //import necessary modules
 import express from 'express';
 
-import employeesRoutes from "./src/routes/employeesRoutes.js";
-import branchesRoutes from "./src/routes/branchesRoutes.js";
-import salesRoutes from "./src/routes/salesRoutes.js";
+import branchesRoutes from './src/routes/branchesRoutes.js';
+import brandsRoutes from './src/routes/brandsRoutes.js';
+import customersRoutes from './src/routes/customersRoutes.js';
+import employeesRoutes from './src/routes/employeesRoutes.js';
+//import inventoryRoutes from './src/routes/inventoryRoutes.js';
+import membershipsRoutes from './src/routes/membershipsRoutes.js';
+//import reviewsRoutes from './src/routes/reviewsRoutes.js';
+import salesRoutes from './src/routes/salesRoutes.js';
+//import watchesRoutes from './src/routes/watchesRoutes.js';
 
 //Create a new express app instance
 const app = express();
@@ -12,10 +30,15 @@ const app = express();
 //middlewares
 app.use(express.json());
 
-app.use("/api/employees/", employeesRoutes);
-app.use("/api/branches/", branchesRoutes);
-app.use("/api/sales/", salesRoutes);
-
+app.use("/api/branches", branchesRoutes);
+app.use("/api/brands", brandsRoutes);
+app.use("/api/customers", customersRoutes);
+app.use("/api/employees", employeesRoutes);
+//app.use("/api/inventory", inventoryRoutes);
+app.use("/api/memberships", membershipsRoutes);
+//app.use("/api/reviews", reviewsRoutes);
+app.use("/api/sales", salesRoutes);
+//app.use("/api/watches", watchesRoutes);
 
 //Define the port for the server
 export default app;
