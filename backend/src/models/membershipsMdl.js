9ce94c46501b638b506d7f1aad94f7715984be10
AlexDev07 <alekeymurcia@gmail.com>
2025-03-21 20:27:38 -0600

    npm run dev agregado y modificaciones cruds míos

--- backend/src/models/membershipsMdl.js
+++ backend/src/models/membershipsMdl.js
@@ -11,34 +11,21 @@ import { Schema, model } from "mongoose";
 const MembershipSchema = new Schema({
    membershipTier: {
       type: String,
-      required: [true, "membershipTier is required"],
-      enum: {
-         values: ['Bronze', 'Plata', 'Oro'],
-         message: '{VALUE} is not a valid membership tier'
-      }
+      required: [true, "membershipTier is required"]
+      
    },
    price: {
       type: Number,
       required: [true, "price is required"],
       min: [0, "price cannot be less than 0"],
    },
-   benefits: [{
+   benefits: {
       type: String,
       validate: {
          validator: function (v) {
             return v.length > 0;
          },
          message: "At least one benefit is required"
-      }
-   }],
-   startDate: {
-      type: Date,
-      required: [true, "startDate is required"],
-      validate: {
-         validator: function (v) {
-            return v >= new Date();
-         },
-         message: "The start date must be greater than or equal to the current date"
       }
    },
    discount: {
