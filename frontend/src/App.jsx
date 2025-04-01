df9548789fb2e410c0c8927737b75b1ae3f9a5f9
AlexDev07 <alekeymurcia@gmail.com>
2025-04-01 15:44:43 -0600

    agregar componentes Header y Footer en la aplicación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import Footer from './components/footer'
+import Header from './components/header'
 import Home from './pages/homePage'
 function App() {
 
@@ -7,12 +9,13 @@ function App() {
   return (
     <>
       <Router>
-        <Nav/>
+        <Header/>
         <div className='app-container'>
           <Routes>
             <Route path='/' element={<Home/>}/>
           </Routes>
         </div>
+        <Footer/>
       </Router>
     </>
   )
