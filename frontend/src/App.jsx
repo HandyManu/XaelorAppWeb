05c63b382cbee10d9d8691265db2739761f9c989
Manuel <20220416@ricaldone.edu.sv>
2025-04-01 15:02:10 -0600

    Inicie el login ?

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import Home from './pages/homePage'
+import LogIn from './pages/LogIn'
 function App() {
 
   //Here are the routes for the app
@@ -11,6 +12,7 @@ function App() {
         <div className='app-container'>
           <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<LogIn/>}/>
           </Routes>
         </div>
       </Router>
