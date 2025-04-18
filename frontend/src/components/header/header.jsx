80cc4eadc77bc8cc38942fa9672bbb4cf680b5c9
AlexDev07 <alekeymurcia@gmail.com>
2025-04-17 21:21:28 -0600

    Añadir dependencias de Tailwind CSS y PostCSS; actualizar estructura del componente Header y estilos CSS

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // Cambia Navigate por useNavigate
 import './header.css';
 import SearchBox from './searchBox/searchBox.jsx'
 
-function showHeader() {
+
+function Header() {
     const navigate = useNavigate(); // Hook para navegar programáticamente
+
+    useEffect(() => {
+        // Cargar scripts externos
+        const scriptJQuery = document.createElement('script');
+        scriptJQuery.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+        scriptJQuery.async = true;
+        document.body.appendChild(scriptJQuery);
+
+        const scriptSwiper = document.createElement('script');
+        scriptSwiper.src = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js';
+        scriptSwiper.async = true;
+        document.body.appendChild(scriptSwiper);
+
+        const scriptLocal = document.createElement('script');
+        scriptLocal.src = './header.js';
+        scriptLocal.defer = true;
+        document.body.appendChild(scriptLocal);
+
+        // Limpieza al desmontar el componente
+        return () => {
+            document.body.removeChild(scriptJQuery);
+            document.body.removeChild(scriptSwiper);
+            document.body.removeChild(scriptLocal);
+        };
+    }, []);
 
     const handleLoginClick = () => {
         navigate('/login'); // Navega a la ruta de login
@@ -12,11 +38,12 @@ function showHeader() {
 
     return (
         <header className="header">
-            <div className="logo">
-                <img src="/Logo.svg" alt="Logo" />
-                <h2>Xælör</h2>
-            </div>
             <nav>
+                <div className="logo">
+                    <img src="/Logo.svg" alt="Logo" />
+                    <h2>Xælör</h2>
+                </div>
+
                 <ul>
                     <li><Link to="/">Inicio</Link></li>
                     <li><Link to="/watches">Relojes</Link></li>
@@ -25,11 +52,13 @@ function showHeader() {
                     <li><Link to="/last-publishes">Últimos Lanzamientos</Link></li>
                     <li><Link to="/about-us">Sobre Nosotros</Link></li>
                 </ul>
+
+                <SearchBox />
+                <button className="Login" onClick={handleLoginClick}>Login</button>
             </nav>
-            <SearchBox />
-            <button className="Login" onClick={handleLoginClick}>Login</button>
+
         </header>
     );
 }
 
-export default showHeader;
+export default Header;
