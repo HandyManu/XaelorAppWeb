8e1a760e8e3273b8217d162de5d43639dee587b0
AlexDev07 <alekeymurcia@gmail.com>
2025-04-21 21:17:16 -0600

    Refactor App component to use useLocation for conditional rendering of Header and Footer; update header styles for consistency.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,36 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/header/header.jsx'
 import Footer from './components/footer/footer.jsx'
 import Home from './pages/homePage.jsx'
 import LogIn from './pages/LogIn.jsx'
-function App() {
 
-  //Here are the routes for the app
+// Creamos un componente wrapper para usar useLocation
+function AppContent() {
+  const location = useLocation();
+  const showHeader = location.pathname !== '/login';
+  const showFooter = location.pathname !== '/login';
+
   return (
-    <div>
-
-      <Router>
-        <Header />
-        <div className='app-container'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<LogIn />} />
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
-
-    </div>
-  )
+    <>
+      {showHeader && <Header />}
+      <div className='app-container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<LogIn />} />
+        </Routes>
+      </div>
+      {showFooter && <Footer />}
+    </>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+export default App;
