529e625e7a12b5196f58160c351ce655c381414f
AlexDev <alekeymurcia@gmail.com>
2025-06-23 11:22:12 -0600

    Ya hace lo de verificar el código el solito

--- frontend/frontend-public/config.jsx
+++ frontend/frontend-public/config.jsx
@@ -1,7 +1,9 @@
 export const config = {
     api: {
-        API_BASE: "http://localhost:3333/api"
+        API_BASE: "http://localhost:3333/api",
         //API_BASE: import.meta.env.VITE_API_BASE
+        FRONTEND_URL: "http://localhost:5173",
+        //PAGE_URL: import.meta.env.VITE_PAGE_URL,
     },
 
 };
